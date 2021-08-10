9acaef089b8e45fe66ab0ba783e2ba0aaa7e3152
Asalef <asalef10@gmail.com>
2021-08-10 10:45:32 +0300

    a

--- server/rauting/rautingStudents.js
+++ server/rauting/rautingStudents.js
@@ -1,8 +1,10 @@
-const studentRouter = require('express').Router()
-const studentControllers = require('../controlers/studentControlers')
-studentRouter.get('/',studentControllers.getAllStudents)
-studentRouter.post('/',studentControllers.postStudent)
-studentRouter.put('/:id',studentControllers.upDateById)
-studentRouter.delete('/:id',studentControllers.deleteById)
+const studentRouter = require('express').Router();
+const studentControllers = require('../controlers/studentControlers');
+studentRouter.get('/', (req, res) => {
+  res.send('Hello Ai');
+});
+studentRouter.post('/', studentControllers.postStudent);
+studentRouter.put('/:id', studentControllers.upDateById);
+studentRouter.delete('/:id', studentControllers.deleteById);
 
-module.exports = studentRouter
+module.exports = studentRouter;
